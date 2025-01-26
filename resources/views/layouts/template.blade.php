6f66dae2679bb37b55a953b1a3b9123c95f64d40
rasyayoga <firmansyahyoga03@gmail.com>
2025-01-26 18:23:01 +0700

    change icon website

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/7692/7692809.png" type="image/x-icon"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>To Do List</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-light navbar-light">
